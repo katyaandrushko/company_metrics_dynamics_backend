0372fc217772ab15d1384121d201ea4fad56e5fd
katyaandrushko <katyaandrushko14@gmail.com>
2024-07-23 11:06:12 +0300

    Update server ts

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import config from './config/config'
 import UserRoute from './users/UserRoute'
 import db from './config/db'
 import cors from 'cors'
+import CompanyRoute from './company/CompanyRoute'
+import MetricsRoute from './metrics/MetricsRoute'
 
 const app = express()
 app.use(express.json())
@@ -10,6 +12,8 @@ app.use(cors())
 db()
 
 app.use('/api/users', UserRoute)
+app.use('/companies', CompanyRoute)
+app.use('/metrics', MetricsRoute)
 
 app.listen(config.port, () => {
    console.log(`Server is running on port ${config.port}`)
