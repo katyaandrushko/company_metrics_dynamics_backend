3508e5113378002c4ad119ec27b9869919de010f
katyaandrushko <katyaandrushko14@gmail.com>
2024-07-23 10:59:19 +0300

    Add users functionality

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const config = {
    port: process.env.PORT,
    mongoUrl: process.env.MONGO_URL,
    production: process.env.PRODUCTION,
+   jwtSecret: process.env.JWT_SECRET,
 }
 
 export default config
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import config from './config/config'
+import UserRoute from './users/UserRoute'
+import db from './config/db'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+db()
+
+app.use('/api/users', UserRoute)
+
+app.listen(config.port, () => {
+   console.log(`Server is running on port ${config.port}`)
+})
